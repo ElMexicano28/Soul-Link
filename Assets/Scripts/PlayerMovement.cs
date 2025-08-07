ac0d8668cf03f788904aeaccad9f9ffaf30756f8
ElMexicano28 <114520844+ElMexicano28@users.noreply.github.com>
2025-08-07 19:05:20 -0400

    Cinemachine & Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(PauseController.isPaused)
+        {
+            rb.linearVelocity = Vector2.zero; //stop movement
+            // Animator.SetBool("isWalking", false);
+            return;
+        }
+        */
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
